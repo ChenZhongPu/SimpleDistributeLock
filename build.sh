16254f5041e04a829ce6320e094aa60cf49e7865
ChenZhongPu <chenloveit@gmail.com>
2016-03-23 14:54:24 +0800

    update shell

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ fi
 
 mkdir classout
 
-export CLASSPATH=gson-2.6.2.jar:hamcrest-core-1.3.jar:junit-4.12.jar:$CLASSPATH
+export CLASSPATH=gson-2.6.2.jar:hamcrest-core-1.3.jar:junit-4.12.jar:log4j-api-2.3.jar:log4j-core-2.3.jar:$CLASSPATH
 
 javac -d classout @sources.txt
+
+cp log4j2.xml classout/
---
+++ runRTest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -cp gson-2.6.2.jar:hamcrest-core-1.3.jar:junit-4.12.jar:log4j-api-2.3.jar:log4j-core-2.3.jar:classout/ com.team6.sjtu.test.RMain
